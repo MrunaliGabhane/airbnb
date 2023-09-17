9d087e2445e78d74b5bab10411ddcb958df09ae2
MrunaliGabhane <mrunagabhane@gmail.com>
2023-09-17 22:34:02 +0530

    upload potos from your device

--- api/index.js
+++ api/index.js
@@ -5,6 +5,10 @@ const bcrypt = require("bcryptjs");
 const jwt = require("jsonwebtoken");
 const User = require("./models/User.js");
 const cookieParser = require("cookie-parser");
+const imageDownloader = require("image-downloader");
+const multer = require('multer')
+const fs = require('fs')
+
 require("dotenv").config();
 const app = express();
 
@@ -14,6 +18,7 @@ const jwtSecret = "dsfgrefftrfb75adc";
 
 app.use(express.json());
 app.use(cookieParser());
+app.use("/uploads", express.static(__dirname + "/uploads"));
 app.use(
   cors({
     credentials: true,
@@ -65,23 +70,50 @@ app.post("/login", async (req, res) => {
   }
 });
 
-app.listen(8080);
-
 app.get("/profile", (req, res) => {
   const { token } = req.cookies;
   if (token) {
-    jwt.verify(token, jwtSecret, {}, async(err, userData) => {
+    jwt.verify(token, jwtSecret, {}, async (err, userData) => {
       if (err) throw err;
-    const {name, email, _id} = await User.findById(userData.id) 
-    res.json({name, email, _id});
+      const { name, email, _id } = await User.findById(userData.id);
+      res.json({ name, email, _id });
     });
   } else {
     res.json(null);
   }
 });
 
-app.post('/logout', (req, res) => {
-  res.cookie('token', "").json(true)
-})
+app.post("/logout", (req, res) => {
+  res.cookie("token", "").json(true);
+});
 
-//2
+//console.log({ __dirname });
+app.post("/upload-by-link", async (req, res) => {
+  const { link } = req.body;
+  const newName = "photo" + Date.now() + ".jpg";
+  await imageDownloader.image({
+    url: link,
+    dest: __dirname + "/uploads/" + newName,
+  });
+  res.json(newName);
+});
+
+
+const photosMiddleware = multer({ dest: "uploads/" });
+app.post("/upload", photosMiddleware.array("photos", 100), (req, res) => {
+  const uploadedFiles = [];
+  for(let i=0; i<req.files.length; i++){
+    const {path, originalname} = req.files[i];
+   const parts =  originalname.split('.')
+   const ext = parts[parts.length - 1]
+    const newPath = path + '.' + ext;
+    //console.log(newPath)
+    fs.renameSync(path,newPath)
+    uploadedFiles.push(newPath.replace('uploads\\',''))
+  }
+  res.json(uploadedFiles);
+});
+
+app.listen(8080);
+
+//3:22
